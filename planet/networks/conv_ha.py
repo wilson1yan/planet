18424af78b25612fea4f06120843c7dc757ac396
Wilson Yan <wilson1.yan@berkeley.edu>
2020-02-01 16:22:57 -0800

    no decoder again

--- planet/networks/conv_ha.py
+++ planet/networks/conv_ha.py
@@ -38,17 +38,17 @@ def encoder(obs):
   return hidden
 
 
-def decoder(state, data_shape):
-  """Compute the data distribution of an observation from its state."""
-  kwargs = dict(strides=2, activation=tf.nn.relu)
-  hidden = tf.layers.dense(state, 1024, None)
-  hidden = tf.reshape(hidden, [-1, 1, 1, hidden.shape[-1].value])
-  hidden = tf.layers.conv2d_transpose(hidden, 128, 5, **kwargs)
-  hidden = tf.layers.conv2d_transpose(hidden, 64, 5, **kwargs)
-  hidden = tf.layers.conv2d_transpose(hidden, 32, 6, **kwargs)
-  mean = tf.layers.conv2d_transpose(hidden, 3, 6, strides=2)
-  assert mean.shape[1:].as_list() == [64, 64, 3], mean.shape
-  mean = tf.reshape(mean, tools.shape(state)[:-1] + data_shape)
-  dist = tfd.Normal(mean, 1.0)
-  dist = tfd.Independent(dist, len(data_shape))
-  return dist
+# def decoder(state, data_shape):
+#   """Compute the data distribution of an observation from its state."""
+#   kwargs = dict(strides=2, activation=tf.nn.relu)
+#   hidden = tf.layers.dense(state, 1024, None)
+#   hidden = tf.reshape(hidden, [-1, 1, 1, hidden.shape[-1].value])
+#   hidden = tf.layers.conv2d_transpose(hidden, 128, 5, **kwargs)
+#   hidden = tf.layers.conv2d_transpose(hidden, 64, 5, **kwargs)
+#   hidden = tf.layers.conv2d_transpose(hidden, 32, 6, **kwargs)
+#   mean = tf.layers.conv2d_transpose(hidden, 3, 6, strides=2)
+#   assert mean.shape[1:].as_list() == [64, 64, 3], mean.shape
+#   mean = tf.reshape(mean, tools.shape(state)[:-1] + data_shape)
+#   dist = tfd.Normal(mean, 1.0)
+#   dist = tfd.Independent(dist, len(data_shape))
+#   return dist
