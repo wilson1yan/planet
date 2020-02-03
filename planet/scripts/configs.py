87161dc51c08d224d148443196e35ea875b71114
Wilson Yan <wilson1.yan@berkeley.edu>
2020-02-02 19:34:28 -0800

    new pixel stack wrapper

--- planet/scripts/configs.py
+++ planet/scripts/configs.py
@@ -79,7 +79,7 @@ def debug(config, params):
 
 def _data_processing(config, params):
   config.batch_shape = params.get('batch_shape', (128, 2))
-  config.num_chunks = params.get('num_chunks', 1)
+  config.num_chunks = int(params.get('num_chunks', 1))
   config.stack_obs = params.get('stack_obs', False)
   config.n_stack_history = params.get('n_stack_history', 2)
   image_bits = params.get('image_bits', 5)
